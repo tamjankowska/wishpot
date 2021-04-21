3e52d58f8ba911f686823a4cb06333175515eab6
Tam <tam.jankowska@gmail.com>
2021-04-21 12:43:09 +0100

    regenerated pots app and created db superuser

--- wishpot/settings.py
+++ wishpot/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import json
-import certifi
-from six.moves.urllib import request
-from cryptography.x509 import load_pem_x509_certificate
-from cryptography.hazmat.backends import default_backend
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,34 +139,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH0_DOMAIN = 'yankov.eu.auth0.com'
-API_IDENTIFIER = 'https://wishpot-api'
-PUBLIC_KEY = None
-JWT_ISSUER = None
-
-if AUTH0_DOMAIN:
-    jsonurl = request.urlopen('https://' + AUTH0_DOMAIN + '/.well-known/jwks.json', cafile = certifi.where())
-    jwks = json.loads(jsonurl.read().decode('utf-8'))
-    cert = '-----BEGIN CERTIFICATE-----\n' + jwks['keys'][0]['x5c'][0] + '\n-----END CERTIFICATE-----'
-    certificate = load_pem_x509_certificate(cert.encode('utf-8'), default_backend())
-    PUBLIC_KEY = certificate.public_key()
-    JWT_ISSUER = 'https://' + AUTH0_DOMAIN + '/'
-
-
-def jwt_get_username_from_payload_handler(payload):
-    return 'auth0user'
-
-
-JWT_AUTH = {
-    'JWT_PAYLOAD_GET_USERNAME_HANDLER': jwt_get_username_from_payload_handler,
-    'JWT_PUBLIC_KEY': PUBLIC_KEY,
-    'JWT_ALGORITHM': 'RS256',
-    'JWT_AUDIENCE': API_IDENTIFIER,
-    'JWT_ISSUER': JWT_ISSUER,
-    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-}
-
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
-)
--- wishpot/urls.py
+++ wishpot/urls.py
@@ -3,6 +3,4 @@ from pots import views
 
 
 urlpatterns = [
-    url(r'^api/public/', views.public),
-    url(r'^api/private/', views.private)
 ]
