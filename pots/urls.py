e64a8ff4a10917f3f8d7253eca41e09d32d97fbc
Tam <tam.jankowska@gmail.com>
2021-04-28 11:34:10 +0100

    python installed locally, urls fixed

--- pots/urls.py
+++ pots/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignupView.as_view(), name="signup"),
+    path('user/signup/', views.SignupView.as_view(), name="signup"),
     path('dashboard/', views.DashboardView.as_view(), name="dashboard")
 ]
